dcc16cd276834ad6b069757cd0af5663e1a10c42
Peter Vanhee <peter.vanhee@gmail.com>
2017-03-20 12:52:48 +0000

    Using redux within react using a dummy component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import { createStore } from 'redux';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// Declare a dumb React component as a function (available since React 0.14)
+const Counter = ({
+	value,
+	onIncrement,
+	onDecrement
+}) => (
+	<div>
+		<h1>{value}</h1>
+		<button onClick={onIncrement}>+</button>
+		<button onClick={onDecrement}>-</button>
+	</div>
 );
+
+const store = createStore(counter);
+
+const render = () => {
+	ReactDOM.render(
+	  <Counter
+	  	value={store.getState()}
+	  	onIncrement={() =>
+	  		store.dispatch({
+	  			type: 'INCREMENT'
+	  		})
+	  	}
+	  	onDecrement={() =>
+	  		store.dispatch({
+	  			type: 'DECREMENT'
+	  		})
+	  	}
+	  />,
+	  document.getElementById('root')
+	);
+};
+
+store.subscribe(render);
+render();
